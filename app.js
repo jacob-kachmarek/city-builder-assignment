d62e9a6c111be7ce723a07e184721a225630ae21
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-01-13 09:45:08 -0800

    added function for tracking user input stats called displayStats

--- app.js
+++ app.js
@@ -10,23 +10,36 @@ const guestDropdown = document.getElementById('guest-dropdown');
 const sloganInput = document.getElementById('slogan-input');
 const sloganButton = document.getElementById('slogan-button');
 const locationStat = document.getElementById('location-stat');
+const partyStat = document.getElementById('party-stat');
+const guestStat = document.getElementById('guest-stat');
 
 /* State */
 let locationCount = 0;
+let partyCount = 0;
+let guestCount = 0;
 /* Events */
 locationDropdown.addEventListener('change', () => {
     locationImg.src = `assets/location-${locationDropdown.value}.jpg`;
     locationCount++;
-    locationStat.textContent = locationCount;
+    displayStats();
 });
 
 partyDropdown.addEventListener('change', () => {
     partyImg.src = `assets/party-${partyDropdown.value}.jpg`;
+    partyCount++;
+    displayStats();
 });
 
 guestDropdown.addEventListener('change', () => {
     guestImg.src = `assets/guest-${guestDropdown.value}.jpg`;
+    guestCount++;
+    displayStats();
 });
 /* Display Functions */
+function displayStats() {
+    locationStat.textContent = locationCount;
+    partyStat.textContent = partyCount;
+    guestStat.textContent = guestCount;
+}
 
 // (don't forget to call any display functions you want to run on page load!)
