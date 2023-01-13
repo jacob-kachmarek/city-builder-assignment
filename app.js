89c0cc5e62aaa9d5117873cc0528da8bdb85fd7b
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-01-13 11:47:15 -0800

    added function for listing user inputs and displaying them to the user

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const partyDropdown = document.getElementById('party-dropdown');
 const guestDropdown = document.getElementById('guest-dropdown');
 const sloganInput = document.getElementById('slogan-input');
 const sloganButton = document.getElementById('slogan-button');
+const sloganEl = document.getElementById('slogans');
 const locationStat = document.getElementById('location-stat');
 const partyStat = document.getElementById('party-stat');
 const guestStat = document.getElementById('guest-stat');
@@ -17,6 +18,7 @@ const guestStat = document.getElementById('guest-stat');
 let locationCount = 0;
 let partyCount = 0;
 let guestCount = 0;
+let slogans = [];
 /* Events */
 locationDropdown.addEventListener('change', () => {
     locationImg.src = `assets/location-${locationDropdown.value}.jpg`;
@@ -35,11 +37,32 @@ guestDropdown.addEventListener('change', () => {
     guestCount++;
     displayStats();
 });
+
+sloganButton.addEventListener('click', () => {
+    slogans.push(sloganInput.value);
+    sloganInput.value = '';
+    displaySlogans();
+});
 /* Display Functions */
 function displayStats() {
     locationStat.textContent = locationCount;
     partyStat.textContent = partyCount;
     guestStat.textContent = guestCount;
+}
+
+function displaySlogans() {
+    sloganEl.textContent = '';
+    for (let slogan of slogans) {
+        const newElement = renderSlogan(slogan);
+        sloganEl.append(newElement);
+    }
+}
+
+function renderSlogan(slogan) {
+    const newElement = document.createElement('p');
+    newElement.classList.add('slogan');
+    newElement.textContent = slogan;
+    return newElement;
 }
 
 // (don't forget to call any display functions you want to run on page load!)
