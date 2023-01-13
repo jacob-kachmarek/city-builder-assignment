05043981cead5b377a2dc923f26fd2f48f52050d
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-01-12 18:22:53 -0800

    pulled dom elements into js and added event listeners for the dropdowns

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 /* Imports */
 
 /* Get DOM Elements */
+const locationImg = document.getElementById('location-image');
+const partyImg = document.getElementById('party-image');
+const guestImg = document.getElementById('guest-image');
+const locationDropdown = document.getElementById('location-dropdown');
+const partyDropdown = document.getElementById('party-dropdown');
+const guestDropdown = document.getElementById('guest-dropdown');
+const sloganInput = document.getElementById('slogan-input');
+const sloganButton = document.getElementById('slogan-button');
 
 /* State */
 
 /* Events */
+locationDropdown.addEventListener('change', () => {
+    locationImg.src = `assets/location-${locationDropdown.value}.jpg`;
+});
 
+partyDropdown.addEventListener('change', () => {
+    partyImg.src = `assets/party-${partyDropdown.value}.jpg`;
+});
+
+guestDropdown.addEventListener('change', () => {
+    guestImg.src = `assets/guest-${guestDropdown.value}.jpg`;
+});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
